d287cecccc389753ca6998e4cee46072645f1f4a
DutchmanNL <rdrozda86@gmail.com>
2020-04-17 10:32:41 +0200

    coding style & formatting

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
  * Created with @iobroker/create-adapter v1.16.0
@@ -6,12 +6,12 @@
 
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
-const utils = require("@iobroker/adapter-core");
-const http_request = require("request");
-const state_attr = require(__dirname + "/lib/state_attr.js"), all_meters = [];
-let user, pass, polling = {};
+const utils = require('@iobroker/adapter-core');
+const http_request = require('request');
+const state_attr = require(__dirname + '/lib/state_attr.js'), all_meters = [], polling = {};
+let user, pass;
 // Load your modules here, e.g.:
-// const fs = require("fs");
+// const fs = require('fs');
 
 class Discovergy extends utils.Adapter {
 
@@ -21,10 +21,10 @@ class Discovergy extends utils.Adapter {
 	constructor(options) {
 		super({
 			...options,
-			name: "discovergy",
+			name: 'discovergy',
 		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("unload", this.onUnload.bind(this));
+		this.on('ready', this.onReady.bind(this));
+		this.on('unload', this.onUnload.bind(this));
 	}
 
 	/**
@@ -32,29 +32,28 @@ class Discovergy extends utils.Adapter {
 	 */
 	async onReady() {
 
-		this.setState("info.connection", false, true);
-		this.log.info("Discovergy Adapter startet, trying to discover meters associated with your account");
-
+		this.setState('info.connection', false, true);
+		this.log.info('Discovergy Adapter startet, trying to discover meters associated with your account');
 		user = this.config.Username;
 		
 		// Check if credentials are not empty and decrypt stored password
-		if (user !== "" && pass !== ""){
-			this.getForeignObject("system.config", (err, obj) => {
+		if (user !== '' && pass !== ''){
+			this.getForeignObject('system.config', (err, obj) => {
 				if (obj && obj.native && obj.native.secret) {
 				//noinspection JSUnresolvedVariable
 					pass = this.decrypt(obj.native.secret, this.config.Password);
 				} else {
 				//noinspection JSUnresolvedVariable
-					pass = this.decrypt("Zgfr56gFe87jJOM", this.config.Password);
+					pass = this.decrypt('Zgfr56gFe87jJOM', this.config.Password);
 				}
 				
 				// Make a call to discovergai API and get a list of all meters
-				this.doDiscovergyCall(user, pass, "meters", "");
+				this.doDiscovergyCall(user, pass, 'meters', '');
 		
 			});
 		} else {
-			this.log.error("*** Adapter deactivated, credentials missing in Adaptper Settings !!!  ***");
-			this.setForeignState("system.this." + this.namespace + ".alive", false);
+			this.log.error('*** Adapter deactivated, credentials missing in Adaptper Settings !!!  ***');
+			this.setForeignState('system.this.' + this.namespace + '.alive', false);
 		}
 	}
 
@@ -64,7 +63,7 @@ class Discovergy extends utils.Adapter {
 		http_request(requestUrl, (error, response, body) => {
 	
 			if (!error && response.statusCode === 200) {
-				this.setState("info.connection", true, true);
+				this.setState('info.connection', true, true);
 				// we got a response
 	
 				// Retrieve all meter objects from Discovergy API
@@ -77,46 +76,46 @@ class Discovergy extends utils.Adapter {
 
 					// Create device and info channel
 					this.log.debug(JSON.stringify(objArray[i]));
-					this.createDevice(objArray[i]["serialNumber"]);
-					this.createChannel(objArray[i]["serialNumber"],"info");
+					this.createDevice(objArray[i]['serialNumber']);
+					this.createChannel(objArray[i]['serialNumber'],'info');
 
 					// Create info channel for alle meter devices
 					for (const x in objArray[i]){
 
 						if (state_attr[x] === undefined){
-							this.log.error("State type : " + x + " unknown, send this information to the developer ==> " + x + " : " + JSON.stringify(objArray[i][x]));
+							this.log.error('State type : ' + x + ' unknown, send this information to the developer ==> ' + x + ' : ' + JSON.stringify(objArray[i][x]));
 						} else {
-							this.doStateCreate(objArray[i]["serialNumber"] + ".info." + x,state_attr[x].name,state_attr[x].type,state_attr[x].role,state_attr[x].read,state_attr[x].unit,state_attr[x].write);
-							this.setState(objArray[i]["serialNumber"] + ".info." + x, objArray[i][x], true);
+							this.doStateCreate(objArray[i]['serialNumber'] + '.info.' + x,state_attr[x].name,state_attr[x].type,state_attr[x].role,state_attr[x].read,state_attr[x].unit,state_attr[x].write);
+							this.setState(objArray[i]['serialNumber'] + '.info.' + x, objArray[i][x], true);
 						}
 					}
 
-					if (objArray[i]["type"] !== "RLM") {
-						const serialNumber = objArray[i]["serialNumber"];
-						const meterId = objArray[i]["meterId"];
+					if (objArray[i]['type'] !== 'RLM') {
+						const serialNumber = objArray[i]['serialNumber'];
+						const meterId = objArray[i]['meterId'];
 						const test = {
-							"meterId": meterId,
-							"serialNumber":  serialNumber
+							'meterId': meterId,
+							'serialNumber':  serialNumber
 						};
 					
 						// Build array with all meter (used later for data polling)
 						all_meters.push(test);
-						// this.doDiscovergyMeter(user, pass, "last_reading", meterId, serialNumber,pulltype);
+						// this.doDiscovergyMeter(user, pass, 'last_reading', meterId, serialNumber,pulltype);
 					}
 				}
 
-				this.log.debug("All meters : " + JSON.stringify(all_meters));
+				this.log.debug('All meters : ' + JSON.stringify(all_meters));
 				
 				for (const z in all_meters) {
 					this.log.debug(JSON.stringify(all_meters[z]));
-					// this.log.info("Test alle meter run");
-					this.doDiscovergyMeter(user, pass, "last_reading", all_meters[z].meterId, all_meters[z].serialNumber);
-					this.log.info("Discovergy meter found at your account with serial  : " + all_meters[z].serialNumber);
+					// this.log.info('Test alle meter run');
+					this.doDiscovergyMeter(user, pass, 'last_reading', all_meters[z].meterId, all_meters[z].serialNumber);
+					this.log.info('Discovergy meter found at your account with serial  : ' + all_meters[z].serialNumber);
 				}
 
 			} else { // error or non-200 status code
-				this.log.error("Connection_Failed at meter indication run, check your credentials !");
-				this.setState("info.connection", false, true);
+				this.log.error('Connection_Failed at meter indication run, check your credentials !');
+				this.setState('info.connection', false, true);
 			}
 		});
 	}
@@ -124,20 +123,20 @@ class Discovergy extends utils.Adapter {
 	async doStateCreate(state, name, type, role, read, unit, write){
 
 		this.setObjectNotExists(state, {
-			type: "state",
+			type: 'state',
 			common: {
 				name: name,
 				type: type,
 				role: role,
 				read: read,
 				unit: unit,
-				write: false,
+				write: write,
 			},
 			native: {},
 		});
 
 		this.extendObject(state, {
-			type: "state",
+			type: 'state',
 			common: {
 				write: false,
 			},
@@ -150,13 +149,12 @@ class Discovergy extends utils.Adapter {
 		http_request(requestUrl, (error, response, body) => {
 
 			// Run this routine again with iintervall defined in settings
-			const intervall = (this.config.intervall * 1000);
+			const intervall = (1000 * this.config.intervall);
 			// timer
 			polling[serial] = setTimeout( () => {
-				this.log.debug("Timer for meter : " + serial + " with data : " +   ' intervall ');
+				this.log.debug('Timer for meter : ' + serial + ' with data : ' +   ' intervall ');
 				this.doDiscovergyMeter(username, password, endpoint, urlencoded_parameters, serial);
 			}, intervall);
-
 
 			if (!error && response.statusCode === 200) {
 				// we got a response
@@ -169,68 +167,68 @@ class Discovergy extends utils.Adapter {
 					for (const x in data[i]) {
 
 						if (state_attr[x] === undefined){
-							this.log.error("State type : " + x + " unknown, send this information to the developer ==> " + x + " : " + JSON.stringify(data[i][x]));
+							this.log.error('State type : ' + x + ' unknown, send this information to the developer ==> ' + x + ' : ' + JSON.stringify(data[i][x]));
 						} else {
 
 							if (state_attr[x].type !== undefined) {
 
 								switch (x) {
-									case "power":
+									case 'power':
 										if (data[i][x] > 0) {
-											this.doStateCreate(serial + ".Power_Consumption", "Momentanwert jetzige Abnahme", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_Consumption", data[i][x], x);
-											this.calc_factor(serial + ".Power_Delivery", 0, x);
+											this.doStateCreate(serial + '.Power_Consumption', 'Momentanwert jetzige Abnahme', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_Consumption', data[i][x], x);
+											this.calc_factor(serial + '.Power_Delivery', 0, x);
 										} else {
-											this.doStateCreate(serial + ".Power_Delivery", "Momentanwert jetziger Abgabe", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_Delivery", Math.abs(data[i][x]), x);
-											this.calc_factor(serial + ".Power_Consumption", 0, x);										
+											this.doStateCreate(serial + '.Power_Delivery', 'Momentanwert jetziger Abgabe', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_Delivery', Math.abs(data[i][x]), x);
+											this.calc_factor(serial + '.Power_Consumption', 0, x);										
 										}
 	
 										break;
 
-									case "power1":
+									case 'power1':
 										if (data[i][x] > 0) {
-											this.doStateCreate(serial + ".Power_T1_Consumption", "Momentanwert jetzige Abnahme T1", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T1_Consumption", data[i][x], x);
-											this.calc_factor(serial + ".Power_T1_Delivery", 0, x);
+											this.doStateCreate(serial + '.Power_T1_Consumption', 'Momentanwert jetzige Abnahme T1', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T1_Consumption', data[i][x], x);
+											this.calc_factor(serial + '.Power_T1_Delivery', 0, x);
 										} else {
-											this.doStateCreate(serial + ".Power_T1_Delivery", "Momentanwert jetziger Abgabe T1", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T1_Delivery", Math.abs(data[i][x]), x);
-											this.calc_factor(serial + ".Power_T1_Consumption", 0, x);										
+											this.doStateCreate(serial + '.Power_T1_Delivery', 'Momentanwert jetziger Abgabe T1', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T1_Delivery', Math.abs(data[i][x]), x);
+											this.calc_factor(serial + '.Power_T1_Consumption', 0, x);										
 										}
 
 										break;
 	
-									case "power2":
+									case 'power2':
 										if (data[i][x] > 0) {
-											this.doStateCreate(serial + ".Power_T2_Consumption", "Momentanwert jetzige Abnahme T2", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T2_Consumption", data[i][x], x);
-											this.calc_factor(serial + ".Power_T2_Delivery", 0, x);	
+											this.doStateCreate(serial + '.Power_T2_Consumption', 'Momentanwert jetzige Abnahme T2', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T2_Consumption', data[i][x], x);
+											this.calc_factor(serial + '.Power_T2_Delivery', 0, x);	
 										} else {
-											this.doStateCreate(serial + ".Power_T2_Delivery", "Momentanwert jetziger Abgabe T2", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T2_Delivery", Math.abs(data[i][x]), x);
-											this.calc_factor(serial + ".Power_T2_Consumption", 0, x);										
+											this.doStateCreate(serial + '.Power_T2_Delivery', 'Momentanwert jetziger Abgabe T2', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T2_Delivery', Math.abs(data[i][x]), x);
+											this.calc_factor(serial + '.Power_T2_Consumption', 0, x);										
 										}
 
 										break;
 	
-									case "power3":
+									case 'power3':
 										if (data[i][x] > 0) {
-											this.doStateCreate(serial + ".Power_T3_Consumption", "Momentanwert jetzige Abnahme T3", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T3_Consumption", data[i][x], x);
-											this.calc_factor(serial + ".Power_T3_Delivery", 0, x);
+											this.doStateCreate(serial + '.Power_T3_Consumption', 'Momentanwert jetzige Abnahme T3', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T3_Consumption', data[i][x], x);
+											this.calc_factor(serial + '.Power_T3_Delivery', 0, x);
 										} else {
-											this.doStateCreate(serial + ".Power_T3_Delivery", "Momentanwert jetziger Abgabe T3", state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-											this.calc_factor(serial + ".Power_T3_Delivery", Math.abs(data[i][x]), x);
-											this.calc_factor(serial + ".Power_T3_Consumption", 0, x);										
+											this.doStateCreate(serial + '.Power_T3_Delivery', 'Momentanwert jetziger Abgabe T3', state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+											this.calc_factor(serial + '.Power_T3_Delivery', Math.abs(data[i][x]), x);
+											this.calc_factor(serial + '.Power_T3_Consumption', 0, x);										
 										}
 
 										break;
 	
 									default:
 	
-										this.doStateCreate(serial + "." + x, state_attr[x].name, state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
-										this.calc_factor(serial + "." + x, data[i][x], x);
+										this.doStateCreate(serial + '.' + x, state_attr[x].name, state_attr[x].type, state_attr[x].role, state_attr[x].read, state_attr[x].unit, state_attr[x].write);
+										this.calc_factor(serial + '.' + x, data[i][x], x);
 	
 								}
 							}
@@ -239,7 +237,7 @@ class Discovergy extends utils.Adapter {
 				}
 
 			} else { // error or non-200 status code
-				this.log.error("Error retrieving information for : " + serial);
+				this.log.error('Error retrieving information for : ' + serial);
 			}
 		});
 	}
@@ -248,99 +246,99 @@ class Discovergy extends utils.Adapter {
 
 		switch (type) {
 
-			case "energy":
+			case 'energy':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energy1":
+			case 'energy1':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energy2":
+			case 'energy2':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energy3":
+			case 'energy3':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyOut":
+			case 'energyOut':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyOut1":
+			case 'energyOut1':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyOut2":
+			case 'energyOut2':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyOut3":
+			case 'energyOut3':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;	
 			
-			case "energyProducer8":
+			case 'energyProducer8':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyProducer9":
+			case 'energyProducer9':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "energyProducer10":
+			case 'energyProducer10':
 
 				this.setState(state, {val: (value / 10000000000), ack: true});
 				break;
 
-			case "power":
+			case 'power':
 
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "power1":
+			case 'power1':
 
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "power2":
+			case 'power2':
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "power3":
+			case 'power3':
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 		
-			case "voltage":
+			case 'voltage':
 
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "voltage1":
+			case 'voltage1':
 
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "voltage2":
+			case 'voltage2':
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
-			case "voltage3":
+			case 'voltage3':
 				this.setState(state, {val: (value / 1000), ack: true});
 				break;
 
 			default:
-				// this.log.error("Error in case handling of type identificaton : " + state);
+				// this.log.error('Error in case handling of type identificaton : ' + state);
 				this.setState(state, {val: value, ack: true});
 				return;
 		}
@@ -349,11 +347,11 @@ class Discovergy extends utils.Adapter {
 
 	// Function to decrypt passwords
 	decrypt(key, value) {
-		let result = "";
+		let result = '';
 		for (let i = 0; i < value.length; ++i) {
 			result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
 		}
-		this.log.debug("client_secret decrypt ready");
+		this.log.debug('client_secret decrypt ready');
 		return result;
 	}
 
@@ -363,7 +361,7 @@ class Discovergy extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
-			this.log.info("cleaned everything up...");
+			this.log.info('cleaned everything up...');
 			callback();
 		} catch (e) {
 			callback();
