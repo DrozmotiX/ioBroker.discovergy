0c8d4c2a16cdb0a06494acec97cfa07d90deb185
DutchmanNL <none@none.com>
2023-10-31 10:59:11 +0100

    update admin interface to JSON-Config

--- admin/words.js
+++ admin/words.js
@@ -2,49 +2,9 @@
 'use strict';
 
 systemDictionary = {
-	'discovergy adapter settings': {
-		'en': 'Adapter settings for discovergy',
-		'de': 'Adaptereinstellungen für discovergy',
-		'ru': 'Настройки адаптера для discovergy',
-		'pt': 'Configurações do adaptador para discovergy',
-		'nl': 'Adapterinstellingen voor discovergy',
-		'fr': "Paramètres d'adaptateur pour discovergy",
-		'it': "Impostazioni dell'adattatore per discovergy",
-		'es': 'Ajustes del adaptador para discovergy',
-		'pl': 'Ustawienia adaptera dla discovergy'
-	},
-	'Username': {
-		'en': 'Username',
-		'de': 'Benutzername',
-		'ru': 'Вариант 1 - это круто',
-		'pt': 'Opção 1 é legal',
-		'nl': 'Optie 1 is cool',
-		'fr': "L'option 1 est cool",
-		'it': "L'opzione 1 è cool",
-		'es': 'La opción 1 es genial',
-		'pl': 'Opcja 1 jest fajna'
-	},
-	'Password': {
-		'en': 'Password',
-		'de': 'Kennwort',
-		'ru': 'Вариант 2 не',
-		'pt': 'A opção 2 não é',
-		'nl': 'Optie 2 is dat niet',
-		'fr': "L'option 2 n'est pas",
-		'it': "L'opzione 2 no",
-		'es': 'La opción 2 no es',
-		'pl': 'Opcja 2 nie jest'
-	},
-	'intervall': {
-		'en': 'Refresh intervall in second',
-		'de': 'Aktualisierungsintervall in Sekunde',
-		'ru': 'Обновить интервал в секунду',
-		'pt': 'Atualizar intervalo em segundo',
-		'nl': 'Vernieuw intervall in seconde',
-		'fr': "Rafraîchir l'intervalle en seconde",
-		'it': 'Intervallo di aggiornamento al secondo',
-		'es': 'Actualizar intervalo en segundo',
-		'pl': 'Odśwież interwał w sekundę',
-		'zh-cn': '刷新间隔秒'
-	}
+    "Password": {                                    "en": "Password",                                        "de": "Kennwort",                                        "ru": "Вариант 2 не",                                    "pt": "A opção 2 não é",                                 "nl": "Optie 2 is dat niet",                             "fr": "L'option 2 n'est pas",                            "it": "L'opzione 2 no",                                  "es": "La opción 2 no es",                               "pl": "Opcja 2 nie jest",                                "zh-cn": "密码"},
+    "Username": {                                    "en": "Username",                                        "de": "Benutzername",                                    "ru": "Вариант 1 - это круто",                           "pt": "Opção 1 é legal",                                 "nl": "Optie 1 is cool",                                 "fr": "L'option 1 est cool",                             "it": "L'opzione 1 è cool",                              "es": "La opción 1 es genial",                           "pl": "Opcja 1 jest fajna",                              "zh-cn": "用户名"},
+    "discovergy adapter settings": {                 "en": "Adapter settings for discovergy",                 "de": "Adaptereinstellungen für discovergy",             "ru": "Настройки адаптера для discovergy",               "pt": "Configurações do adaptador para discovergy",      "nl": "Adapterinstellingen voor discovergy",             "fr": "Paramètres d'adaptateur pour discovergy",         "it": "Impostazioni dell'adattatore per discovergy",     "es": "Ajustes del adaptador para discovergy",           "pl": "Ustawienia adaptera dla discovergy",              "zh-cn": "用于发现的适配器设置"},
+    "intervall": {                                   "en": "Refresh intervall in second",                     "de": "Aktualisierungsintervall in Sekunde",             "ru": "Обновить интервал в секунду",                     "pt": "Atualizar intervalo em segundo",                  "nl": "Vernieuw intervall in seconde",                   "fr": "Rafraîchir l'intervalle en seconde",              "it": "Intervallo di aggiornamento al secondo",          "es": "Actualizar intervalo en segundo",                 "pl": "Odśwież interwał w sekundę",                      "zh-cn": "刷新间隔秒"},
+    "Your Discovergy Username": {                    "en": "Your Discovergy Username",                        "de": "Ihr Discovergy-Benutzername",                     "ru": "Ваше имя пользователя Discovery",                 "pt": "Seu nome de usuário do Discovergy",               "nl": "Uw Discoverygy-gebruikersnaam",                   "fr": "Votre nom d'utilisateur Discovergy",              "it": "Il tuo nome utente Discovergy",                   "es": "Su nombre de usuario de Discovergy",              "pl": "Twoja nazwa użytkownika Discovery",               "zh-cn": "您的发现用户名"},
 };
